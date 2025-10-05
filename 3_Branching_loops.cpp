e14427aac13f9722eaacaedc7cc3bcf29873f47d
Morgan3839 <morrganika@gmail.com>
2025-10-06 02:24:32 +0700

    убрала массив и диапазонный цикл

--- 3_Branching_loops.cpp
+++ 3_Branching_loops.cpp
@@ -2,7 +2,7 @@
  * Автор: Дорожко Ксения *
  * Вариант: 6            *
  * ***********************/
- 
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -10,18 +10,20 @@ using namespace std;
 int main() {
   double d = 0.025;
   double g = 9.8;
-  double Re = 4.1 * pow(10, 4);
-  double v[] = {0.01, 0.05, 0.1, 0.5, 1.0, 2.0};
-  double Ga, F;
+  double Re = 4.1 * pow(10.0, 4.0);
+  double Ga, F, v;
 
-  for (double koef : v) {
-      Ga = g * pow(d,3) / pow(koef * pow(10, -4), 2);
-      if (Ga < 4 * pow(10, 4)) {
-        F = 0.183 * pow(Re, 0.74) * pow(Ga, -0.256);
-      } else {
-        F = 3.0 * pow(Re, 0.74) * pow(Ga, -0.52);
-      }
-      cout << "F = " << F << "     " << "Ga = " << Ga << endl;
+  for (int i = 0.0; i < 6.0; i++) {
+    cout << "Enter the value of v: ";
+    cin >> v;
+      
+    Ga = g * pow(d, 3.0) / pow(v * pow(10.0, -4.0), 2.0);
+    if (Ga < 4.0 * pow(10.0, 4.0)) {
+      F = 0.183 * pow(Re, 0.74) * pow(Ga, -0.256);
+    } else {
+      F = 3.0 * pow(Re, 0.74) * pow(Ga, -0.52);
+    }
+    cout << "F = " << F << "     " << "Ga = " << Ga << endl;
   }
   return 0;
 }
