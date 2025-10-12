bcdfffbb2ef5c01f13d3342d459cd68897991181
Morgan3839 <morrganika@gmail.com>
2025-10-13 02:40:24 +0700

    убрала auto и vector

--- 4_Array_processing.cpp
+++ 4_Array_processing.cpp
@@ -4,42 +4,49 @@
  * ***********************/
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
   int matrixSize;
   cout << "Enter matrix size: ";
   cin >> matrixSize;
-  
-  vector<vector<int>> G(matrixSize, vector<int>(matrixSize));
+
+  int G[1000][1000];
 
   cout << "Enter matrix elements:" << endl;
   for (int rowIndex = 0; rowIndex < matrixSize; ++rowIndex) {
-    for (int columnIndex = 0; columnIndex < matrixSize; ++columnIndex) {
-      cin >> G[rowIndex][columnIndex];
-    }
+      for (int columnIndex = 0; columnIndex < matrixSize; ++columnIndex) {
+        cin >> G[rowIndex][columnIndex];
+      }
   }
-  
+
   cout << "Original matrix:" << endl;
-  for (const auto& row : G) {
-    for (int element : row) {
-      cout << element << " ";
+  for (int rowIndex = 0; rowIndex < matrixSize; ++rowIndex) {
+    for (int columnIndex = 0; columnIndex < matrixSize; ++columnIndex) {
+      cout << G[rowIndex][columnIndex] << " ";
     }
     cout << endl;
   }
-  
+
   for (int rowIndex = 0; rowIndex < matrixSize; ++rowIndex) {
-    auto maxIterator = max_element(G[rowIndex].begin(), G[rowIndex].end());
-    int maxIndex = distance(G[rowIndex].begin(), maxIterator);
-    swap(G[rowIndex][rowIndex], G[rowIndex][maxIndex]);
-  }
+    int maxIndex = 0;
+      for (int columnIndex = 1; columnIndex < matrixSize; ++columnIndex) {
+        if (G[rowIndex][columnIndex] > G[rowIndex][maxIndex]) {
+          maxIndex = columnIndex;
+        }
+      }
+
+      if (maxIndex != rowIndex) { 
+        G[rowIndex][rowIndex] = G[rowIndex][rowIndex] + G[rowIndex][maxIndex];
+        G[rowIndex][maxIndex] = G[rowIndex][rowIndex] - G[rowIndex][maxIndex];
+        G[rowIndex][rowIndex] = G[rowIndex][rowIndex] - G[rowIndex][maxIndex];
+      }
+    }
 
   cout << "New matrix:" << endl;
-  for (const auto& row : G) {
-    for (int element : row) {
-      cout << element << " ";
+  for (int rowIndex = 0; rowIndex < matrixSize; ++rowIndex) {
+    for (int columnIndex = 0; columnIndex < matrixSize; ++columnIndex) {
+      cout << G[rowIndex][columnIndex] << " ";
     }
     cout << endl;
   }
