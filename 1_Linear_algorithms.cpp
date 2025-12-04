5548dcd2419c80f8ea59378ee7f6f640f2fc9e75
Morgan3839 <morrganika@gmail.com>
2025-12-04 14:24:51 +0700

    исправлен double и cout

--- 1_Linear_algorithms.cpp
+++ 1_Linear_algorithms.cpp
@@ -2,24 +2,23 @@
  * Автор: Дорожко Ксения *
  * Вариант: 6            *
  * ***********************/
- 
+
  #include <iostream>
  using namespace std;
- 
+
  int main() {
-  
-  double a = 3.847, b = 5.33, c = 7.167;
-  
-  double A = acos( (b * b + c * c - a * a) / (2.0 * b * c) );
-  double B = asin( (b * sin(A)) / a);
-  double C = M_PI - A - B;
-  double p = (a + b + c) / 2.0;
-  double C_check = 2.0 * atan( sqrt( (p - a) * (p - b) / (p * (p - c)) ) );
+
+  double a = 3.847, b = 5.33, c = 7.167, A, B, C, p, C_check;
+
+  A = acos( (b * b + c * c - a * a) / (2.0 * b * c) );
+  B = asin( (b * sin(A)) / a);
+  C = M_PI - A - B;
+  p = (a + b + c) / 2.0;
+  C_check = 2.0 * atan( sqrt( (p - a) * (p - b) / (p * (p - c)) ) );
 
   if (abs(C_check-C) < 0.00001) {
-    cout << "A = " << A << endl;
-    cout << "B = " << B << endl;
-    cout << "C = " << C << endl;
+    cout << "A = " << A << endl << "B = " << B << endl << "C = " << C << endl;
+
   } else {
     cout << "no solution" << endl;
   }
