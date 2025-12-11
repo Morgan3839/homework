9b40a057e028bfdae8110aa8028f5832113022af
Morgan3839 <morrganika@gmail.com>
2025-12-11 08:49:57 +0700

    циклы с пред- и постусловием

--- 2_Loops_with_precondition_and_postcondition.cpp
+++ 2_Loops_with_precondition_and_postcondition.cpp
@@ -2,20 +2,33 @@
  * Автор: Дорожко Ксения *
  * Вариант: 6            *
  * ***********************/
- 
+
 #include <iostream>
 #include <cmath>
 using namespace std;
- 
+
 int main() {
-  
+
+  int i = 0;
   double T = 1873.0;
   double Si[] = {0.025, 0.05, 0.075, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7};
   double O;
-  
-  for (double koef : Si) {
-    O = sqrt( pow(10.0, (-31000 / T) + 12.152) / koef );
-    cout << "Si = " << koef << "      " << "O = " << O << endl;
+
+  while (i < 10) {
+    O = sqrt(pow(10.0, (-31000.0 / T) + 12.152) / Si[i]);
+    cout << "Si = " << Si[i] << "      " << "O = " << O << endl;
+    i++;
   }
+
+  cout << "\n--- do-while ---\n\n";
+
+  i = 0;
+  
+  do {
+    O = sqrt(pow(10.0, (-31000.0 / T) + 12.152) / Si[i]);
+    cout << "Si = " << Si[i] << "      " << "O = " << O << endl;
+    i++;
+  } while (i < 10);
+
   return 0;
 }
