cfbcce1e83841a3c64e60a28d20f9c373d6726e1
Morgan3839 <morrganika@gmail.com>
2025-12-19 10:59:01 +0700

    исправление однобуквенной переменной

--- 2_Loops_with_precondition_and_postcondition.cpp
+++ 2_Loops_with_precondition_and_postcondition.cpp
@@ -9,26 +9,26 @@ using namespace std;
 
 int main() {
 
-  int i = 0;
+  int index = 0;
   double T = 1873.0;
   double Si[] = {0.025, 0.05, 0.075, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7};
   double O;
 
-  while (i < 10) {
-    O = sqrt(pow(10.0, (-31000.0 / T) + 12.152) / Si[i]);
-    cout << "Si = " << Si[i] << "      " << "O = " << O << endl;
-    i++;
+  while (index < 10) {
+    O = sqrt(pow(10.0, (-31000.0 / T) + 12.152) / Si[index]);
+    cout << "Si = " << Si[index] << "      " << "O = " << O << endl;
+    index++;
   }
 
   cout << "\n--- do-while ---\n\n";
 
-  i = 0;
-  
+  index = 0;
+
   do {
-    O = sqrt(pow(10.0, (-31000.0 / T) + 12.152) / Si[i]);
-    cout << "Si = " << Si[i] << "      " << "O = " << O << endl;
-    i++;
-  } while (i < 10);
+    O = sqrt(pow(10.0, (-31000.0 / T) + 12.152) / Si[index]);
+    cout << "Si = " << Si[index] << "      " << "O = " << O << endl;
+    index++;
+  } while (index < 10);
 
   return 0;
 }
